5e411a82b84b357eeee47d194392f7a2d82dc3df
Dmytro <dmytro.baitaliuk@y-tree.com>
2023-11-17 11:39:51 +0200

    First task

---
+++ List_task_1.py
@@ -0,0 +1,24 @@
+# 1
+list_1 = [132, 1.5, "qw", True, [1, 2, 3]]
+print(list_1)
+
+# 2
+del list_1[2]
+print(list_1)
+
+# 3
+print(len(list_1))
+
+# 4
+list_1.reverse()
+print(list_1)
+
+# 5
+list_2 = ['My', 'Name']
+print(list_2)
+
+# 6
+list_1.extend(list_2)
+
+# 7
+print(list_1)
---
+++ List_task_2.py
@@ -0,0 +1,11 @@
+# 1
+list_1 = [1, 2, 3, 4, 5]
+list_2 = [6, 7, 8, 9, 10]
+
+# 2 Method +
+list_3 = list_1 + list_2
+print(list_3)
+
+# 3 Method _add__
+list_4 = list_1.__add__(list_2)
+print(list_4)
